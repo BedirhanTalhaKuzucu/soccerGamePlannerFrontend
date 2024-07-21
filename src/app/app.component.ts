9771261fa8e2320ab4fe39a381fea6f0a777fd93
bedirhan kuzucu <btk.kzc@gmail.com>
2024-07-21 15:33:00 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {HeaderComponent} from "./components/header/header.component";
+import {CreateFormComponent} from "./components/create-form/create-form.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, CreateFormComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
